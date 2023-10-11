79adac6bc20d333abf35d64482dcce3877136738
mayankpallai <mayankpallai@gmail.com>
2023-10-11 20:15:54 +0800

    added web-app structure

---
+++ dip-project/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button, buttonVariants} from "@/components/ui/button"
+
+const Home = async () => {  
+  return (
+    <div className = "w-full h-full flex justify-center items-center text-white relative bg-black text-white">
+      <div className = "xl:max-w-[75vw] w-full h-full flex relative">
+        <left
+        <Button className='rounded-full hover:bg-white/20 hover:text-white transition duration-300 px-6 mx-5 my-2 py-5 bg-white text-neutral-950'>Start Training</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Home
